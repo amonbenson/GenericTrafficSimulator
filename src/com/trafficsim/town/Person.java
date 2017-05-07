2fec257e5ae343b5b6ef7ffb02cb66be5b530050
amonbenson141 <amon.k8@aol.de>
2017-05-07 14:03:07 +0200

    Filled the Person and Rout classes with som nice instructions

--- src/com/trafficsim/town/Person.java
+++ src/com/trafficsim/town/Person.java
@@ -1,5 +1,54 @@
 package com.trafficsim.town;
 
 public class Person {
+	
+	private double x, y;
+	private boolean floating; // when a person "floats" it won't be drawn to the town
+	private Route route;
+	
+	public Person(double x, double y) {
+		this.x = x;
+		this.y = y;
+		floating = false;
+		
+		route = null;
+	}
 
+	public Route getRoute() {
+		return route;
+	}
+
+	public void setRoute(Route route) {
+		if (route == null) throw new NullPointerException("Route shouldn't be null. Use removeRoute() to remove the current route");
+		
+		this.route = route;
+	}
+	
+	public void removeRoute() {
+		route = null;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	public boolean isFloating() {
+		return floating;
+	}
+	
+	public void setFloating(boolean floating) {
+		this.floating = floating;
+	}
 }
--- src/com/trafficsim/town/Route.java
+++ src/com/trafficsim/town/Route.java
@@ -1,5 +1,64 @@
 package com.trafficsim.town;
 
 public class Route {
+	
+	private Tile origin, target;
+	private long timeOrigin, timeTarget;
+	
+	public Route(Tile origin, Tile target) {
+		if (origin == null) throw new NullPointerException("origin can't be null.");
+		if (target == null) throw new NullPointerException("target can't be null.");
 
+		this.origin = origin;
+		this.target = target;
+		
+		timeOrigin = -1;
+		timeTarget = -1;
+	}
+	
+	/**
+	 * Will be called when the route starts, e. g. when a person want's to get from Tile A to B. This sets the origin
+	 * time flag, so the total time can be read out later on.
+	 * 
+	 * @param currentTime
+	 * 			Long value representing the current time of the town, used to set the origin time flag.
+	 */
+	public void startRoute(long currentTime) {
+		timeOrigin = currentTime;
+	}
+	
+	/**
+	 * Will be called when the route ends, e. g. when a person arrived at it's target. This sets the target time flag
+	 * So the total time can be read out later on. The function will throw an Exception if the route wasn't started
+	 * before. Also, the time when stopping must be greater or equal than the time when starting.
+	 * 
+	 * @param currentTime
+	 * 			Long value representing the current time of the town, used to set the target time flag.
+	 */
+	public void stopRoute(long currentTime) {
+		if (timeOrigin == -1) throw new IllegalArgumentException("Route must be started first.");
+		if (timeTarget < timeOrigin) throw new IllegalArgumentException("Stop time must be greater than start time.");
+		
+		timeTarget = currentTime;
+	}
+	
+	/**
+	 * Returns the total time that was taken while passing the route.
+	 * 
+	 * @return
+	 * 			Long value to represent the taken time. Returns -1 when route wasn't started
+	 * 			or hasn't finished yet.
+	 */
+	public long getTotalTime() {
+		if (timeTarget == -1) return -1;
+		else return timeTarget - timeOrigin;
+	}
+
+	public Tile getOrigin() {
+		return origin;
+	}
+
+	public Tile getTarget() {
+		return target;
+	}
 }
