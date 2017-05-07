017b000f23d6c5f5e82e37603658a9ba6684527c
lucj26 <jungnickel.luca@gmail.com>
2017-05-07 13:16:20 +0200

    Town

--- src/com/trafficsim/town/Town.java
+++ src/com/trafficsim/town/Town.java
@@ -1,5 +1,24 @@
 package com.trafficsim.town;
 
 public class Town {
-
+	private Tile[][] tiles;
+	private int sizeX, sizeY;
+	
+	public Town(int sizeX, int sizeY) {
+		this.sizeX = sizeX;
+		this.sizeY = sizeY;
+	}
+	
+	public Tile[][] getTiles() {
+		return tiles;
+	}
+	
+	public int getSizeX() {
+		return sizeX;
+	}
+	
+	public int getSizeY() {
+		return sizeY;
+	}
+	
 }
